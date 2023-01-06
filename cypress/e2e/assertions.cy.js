34b30367652739198516addc6285ff3f69da7f39
Irving Juárez <irvingjuarez274@gmail.com>
2023-01-05 19:52:28 -0600

    Using a hook to do something before all the tests

--- cypress/e2e/assertions.cy.js
+++ cypress/e2e/assertions.cy.js
@@ -1,15 +1,14 @@
 describe("Assertions", () => {
-	it("Test one", () => {
+	before(() => {
 		cy.visit("https://platzi.com/blog/codigo-desconocido/")
 		cy.url().should("include", "platzi.com/blog")
+	})
 
+	it("Test one", () => {
 		cy.get("h1").should("be.visible").and("contain.text", "Nuevos Laboratorios Avanzados")
 	})
 
 	it("Test two", () => {
-		cy.visit("https://platzi.com/blog/codigo-desconocido/")
-		cy.url().should("include", "platzi.com/blog")
-
 		cy.get("h1").then(header => {
 			expect(header).to.be.visible
 			expect(header).to.contain.text("Nuevos Laboratorios Avanzados")
