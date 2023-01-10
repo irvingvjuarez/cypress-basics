9648978c3379ebffd15fb413d712c6a2f7622855
Irving Juárez <irvingjuarez274@gmail.com>
2023-01-09 23:47:22 -0600

    Sharing data among more than one test

--- cypress/e2e/extracting.cy.js
+++ cypress/e2e/extracting.cy.js
@@ -1,14 +1,23 @@
 describe("Extracting info from elements", () => {
-	before(() => {
+	let textValue
+
+	beforeEach(() => {
 		cy.visit("https://dev.to/")
+		cy.get("input[placeholder='Search...']").as("search-bar");
 	})
 
 	it("Extracting data", () => {
-		cy.get("input[placeholder='Search...']").as("search-bar");
 		cy.get("@search-bar").type("Three js")
 
 		cy.get("@search-bar").then($input => {
-			expect($input.val()).to.equal("Three js")
+			textValue = $input.val()
+			expect(textValue).to.equal("Three js")
 		})
+	})
+
+	it("Using variables from another test", () => {
+		cy.get("@search-bar").clear()
+		cy.get("@search-bar").type(textValue + " is my passion")
+		// expect(textValue).to.equal("Three js")
 	})
 })
