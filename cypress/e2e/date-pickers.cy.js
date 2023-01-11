07a597304bf1dfeb1e2fe31b0296fb8b70917a0f
Irving Juárez <irvingjuarez274@gmail.com>
2023-01-10 23:02:18 -0600

    Working with dates

--- cypress/e2e/date-pickers.cy.js
+++ cypress/e2e/date-pickers.cy.js
@@ -3,6 +3,12 @@ describe("Date-pickers", () => {
 		cy.visit("https://material.angular.io/components/datepicker/overview")
 		cy.get("datepicker-overview-example").as("picker-container")
 
+		// Clicking the logo to unlock input
+		cy.get("@picker-container")
+			.find("svg")
+			.click()
+
+		// Typing the date on the input
 		cy.get("@picker-container")
 			.find("input")
 			.eq(0)
