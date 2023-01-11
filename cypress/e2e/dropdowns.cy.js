0f1e0404b59eae55bbd517c7c05a82250138944b
Irving Juárez <irvingjuarez274@gmail.com>
2023-01-10 22:15:38 -0600

    Accesing element in a dynamic dropdown

--- cypress/e2e/dropdowns.cy.js
+++ cypress/e2e/dropdowns.cy.js
@@ -1,10 +1,17 @@
 describe("Testing dropdowns", () => {
-	before(() => {
-		cy.visit("https://itera-qa.azurewebsites.net/home/automation")
-	})
-
 	it("Dropdown should work properly", () => {
+		cy.visit("https://itera-qa.azurewebsites.net/home/automation")
 		cy.get(".custom-select").select(10).should("have.value", 10)
 		cy.get(".custom-select").select("Greece").should("have.value", 4)
+	})
+
+	it.only("Dynamic dropdowns", () => {
+		cy.visit("https://react-select.com/home")
+		cy.get("#react-select-6-input").type(" ")
+		cy.get("#react-select-6-listbox").children().children().each($el => {
+			if ($el.text() === "Red") {
+				$el.click()
+			}
+		})
 	})
 })
